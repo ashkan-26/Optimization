738e21f3379c2ea3596aef7270b9c1f5bdf7ce49
ashkan-26 <ashkan.arianejad@khu.ac.ir>
2023-12-06 21:25:35 +0330

    set a constant number for t

--- Optimization.py
+++ Optimization.py
@@ -8,19 +8,19 @@ x, y = sym.symbols('x, y')
 f = 8 * x**2 + 4 * y ** 2 - 9
 Df = sym.Matrix([f]).jacobian(sym.Matrix(list(f.free_symbols)))
 
-# f = lambda x : 7*(x[0]**3) + 5*(x[0]**2) - x[1]**2 + 5*x[0] - 3*x[1] + 10
-f = lambda x : x[0]**2 + x[1]**2 - 2*x[0] - 2*x[1] + 2
-# x0 = np.random.rand(2,) * 10
-x0 = np.array([2000, 8000])
+# Basic Algorithm
+f = lambda x : x[0]**2 + x[1]**2 - 1
+x0 = np.random.rand(2,) * 10
 d = lambda x : Gradient(f)(x)
 e = 1e-5
+# t = minimize_scalar(lambda alpha: f(x0 - alpha * d(x0))).x
+t = 0.5
 while True:
-    t = minimize_scalar(lambda alpha: f(x0 - alpha * d(x0))).x
     x1 = x0 - t*d(x0)
     if np.abs(f(x1) - f(x0)) < e:
         break
     x0 = x1
 
 print(x0)
-# print(t)
+print(t)
 print(f(x0))
