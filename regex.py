884726765b723abf1fed430b5a1b9a454527d639
ashkan-26 <ashkan.arianejad@khu.ac.ir>
2023-12-07 11:29:03 +0330

    alternative method for regex

--- regex.py
+++ regex.py
@@ -1,4 +1,5 @@
 import numpy as np
+import re
 from numdifftools import Gradient
 
 equation = 'x1^2 +x2^2 -1'
@@ -47,3 +48,23 @@ def f(x,eq):
     return ans
 
 print(f([0,0],eq))
+
+#####################################################
+
+def polynomial_to_lambda(polynomial):
+    # Extract variables from the polynomial
+    variables = sorted(list(set(re.findall(r'[a-zA-Z]', polynomial))))
+    
+    # Replace '^' with '**' for Python's power operator
+    polynomial = polynomial.replace('^', '**')
+    
+    # Create lambda function
+    lambda_func = eval('lambda {}: {}'.format(','.join(variables), polynomial))
+    
+    return lambda_func
+
+polynomial = '2*a^3 + 4*a^2 -8*a + 3*b^4 - 2*b^2 - 5*b + 8'
+lambda_func = polynomial_to_lambda(polynomial)
+
+# Now you can call this function with values for a and b
+result = lambda_func(1, 2)  # Substitute a=1, b=2
