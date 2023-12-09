b20f2bf0879f1107279744dadea87e82ca8a7ca1
armin2080 <arminmaddah.a@gmail.com>
2023-12-09 08:35:52 +0330

    converted string to function

--- reg.py
+++ reg.py
@@ -11,3 +11,30 @@ def polynomial(poly: str):
     func = eval('lambda {}: {}'.format(','.join(variables), poly))
     
     return func, len(variables)
+
+
+d = 2
+x = [1,2]
+
+polynomial = '2*x1^3 + 4*x1^2 -8*x1 + 3*x2^4 - 2*x2^2 - 5*x2 + x1*x2 + 8'
+
+# separate all words except xi
+tmp = ""
+for i in polynomial:
+    if i == "x":
+        tmp += i
+    else:
+        tmp += f"{i} "
+polynomial = tmp
+
+# replace xi with x[i-1]
+for i in range(d):
+    polynomial = polynomial.replace(f'x{i+1}',f'x[{i}]')
+
+# replace '^' with '**'
+polynomial = polynomial.replace('^','**')
+
+# defice function using eval
+f = lambda x: eval(polynomial)
+
+print(f([0,1]))
