09a2579201edd109e4d273280e382b8925568bd8
ashkan-26 <ashkan.arianejad@khu.ac.ir>
2023-12-12 09:34:13 +0330

    necessary functions

--- optim.py
+++ optim.py
@@ -1,6 +1,12 @@
 import numpy as np
 import sympy
 
+def inputModifier(fun):     # Input: function with multimple output
+    
+    return lambda y : fun(*y)
+
+    '''Output: function with one input'''
+
 def polynomial(poly: str):      # Input: polynomial function as a string
 
     # Finding all letters(variables), storing them in a set (for uniqueness), converting to a sorted array.
@@ -15,10 +21,20 @@ def polynomial(poly: str):      # Input: polynomial function as a string
     symbolic = sympy.sympify(poly)
 
     # Converting symbolic expression to a lambda function.
-    f = sympy.lambdify(vararr, poly, "numpy")
+    func = sympy.lambdify(vararr, poly, "numpy")
+    f = inputModifier(func)
 
     return f, symbolic, vararr
-    # Output:
-    # f = Polynomial function
-    # symbolic = Symbolic notation of the polynomial. Will be used for computation of Gradient and Hessien matrices.
-    # varrarr = List of polynomial variables.
+
+    ''' 
+    Output:
+    f = Polynomial function
+    symbolic = Symbolic notation of the polynomial. Will be used for computation of Gradient and Hessien matrices.
+    varrarr = List of polynomial variables.
+    '''
+
+# def Gradient(poly): 
+
+#     _, symbolic, x = polynomial(poly)
+
+#     return inputModifier(x, sympy.Matrix([symbolic]).jacobian(x), "numpy")
