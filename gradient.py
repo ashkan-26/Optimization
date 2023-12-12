e8e65623b17c63ad3758b0c3314e26001f24e284
ashkan-26 <ashkan.arianejad@khu.ac.ir>
2023-12-12 10:03:31 +0330

    gradient method core

--- gradient.py
+++ gradient.py
@@ -1,22 +1,23 @@
-from reg import polynomial
 import numpy as np
-from numdifftools import Gradient
-from scipy.optimize import minimize_scalar
+import sympy
+from scipy.optimize import line_search
+from optim import polynomial, inputModifier
 
-f = 'a^4 - 2*a^2 + b^2 + 2*b*c + 2*c^2'
+f = '2*b**3 - 6*b**2 + 3*a**2*b'
+f, symbolic, x = polynomial(f)
 
-f, v = polynomial(f)
+# Algorithm
 
-# Basic Algorithm
-x0 = np.random.rand(v,) * 10
-d = lambda x : Gradient(f)(*x)
-e = 1e-5
-t = 0.00005
+x0 = np.random.rand(len(x),)    # Change the range of x0 by multiplying with different numbers(e.g. *10 or *-1) in the case of non-convergency.
+grad_lambdify = sympy.lambdify(x, sympy.Matrix([symbolic]).jacobian(x), "numpy")
+grad = inputModifier(grad_lambdify)
+d = lambda y : np.ravel(grad(y))
+e = 1e-5    # Will be initialized by user.
 while True:
-    x1 = x0 - t*d(x0)
-    if np.abs(f(*x1) - f(*x0)) < e:
+    t = line_search(f, grad, x0, -1*d(x0))
+    x1 = x0 - t[0] * d(x0)
+    if np.abs(f(x1) - f(x0)) < e:
         break
     x0 = x1
 
-print('Minimum point: ', x1)
-print('Minimal value', f(*x1))
+print(symbolic, '\nMinimum point: ', x1, '\nMinimal value: ', f(x1))
